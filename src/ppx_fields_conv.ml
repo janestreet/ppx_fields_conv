907dbdfe121cd382266735ecf2745415fbb98bed
Xavier Clerc <xclerc@janestreet.com>
2020-08-10 09:54:19 +0100

    v0.15-preview.123.10+171

--- src/ppx_fields_conv.ml
+++ src/ppx_fields_conv.ml
@@ -56,8 +56,8 @@ module A = struct (* Additional AST construction helpers *)
 
   let mod_ ~loc : (string -> structure -> structure_item) =
     fun name structure ->
-      pstr_module ~loc (module_binding ~loc ~name:(Located.mk ~loc name)
-                          ~expr:(pmod_structure ~loc structure))
+    pstr_module ~loc (module_binding ~loc ~name:(Located.mk ~loc (Some name))
+                        ~expr:(pmod_structure ~loc structure))
 
   let sig_item ~loc name typ =
     psig_value ~loc (
@@ -66,8 +66,8 @@ module A = struct (* Additional AST construction helpers *)
 
   let sig_mod ~loc : (string -> signature -> signature_item) =
     fun name signature ->
-      psig_module ~loc (module_declaration ~loc ~name:(Located.mk ~loc name)
-                          ~type_:(pmty_signature ~loc signature))
+    psig_module ~loc (module_declaration ~loc ~name:(Located.mk ~loc (Some name))
+                        ~type_:(pmty_signature ~loc signature))
 
 end
 
